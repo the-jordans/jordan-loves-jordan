2fd6268c6de8b94e0b18102cd7fa07438bf0a0f5
Jordan Fallon <thefetts@gmail.com>
2026-08-08 13:33:16 -0400

    Add version display

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import './App.css'
 
+declare const __APP_VERSION__: string
+const VERSION_NUMBER = __APP_VERSION__
+
 function App() {
   return (
     <main className="centered">
@@ -7,6 +10,9 @@ function App() {
         <span className="line1">JORDAN LOVES JORDAN</span>
         <span className="line2">COMING 2028</span>
       </h1>
+      <div className="version" aria-hidden="true">
+        {`v${VERSION_NUMBER}`}
+      </div>
     </main>
   )
 }
